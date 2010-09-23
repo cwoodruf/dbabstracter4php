b49e2a9bdddfe49d5bf76305b38e319cf21e0c29
cwoodruf@gmail.com <cwoodruf@gmail.com@c5e326fc-19f7-11df-8006-ddd0014e6ec4>
2010-09-23 22:36:10 +0000

    now can do input and output redirect in one command. added echotest.pl test script

--- branches/sfu/cmpt300/a1/shell.c
+++ branches/sfu/cmpt300/a1/shell.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define MAXCARGC 255
 
@@ -11,15 +13,19 @@ int main(int argc, char *argv[], char *envp[])
 {
 	pid_t cpid, w;
 	int status;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 
 	/* for child processes */
 	char cmd[MAXCARGC] = ""; 	/* the raw command */
 	char *infrom; 			/* input redirection */
+	int infd;			/* input redirect file descriptor */
 	char *outto;			/* output redirection */
+	int outfd;			/* output redirect file descriptor */
 	char *ctok;         		/* pointer to next argv token */
 	char *cargv[MAXCARGC];   	/* the argv list created from the command */
 	char cchar;                     /* test first character of argument */
 	int i,cargc;          		/* the argument count */
+	int redirection;		/* we found a | > < character */
 
 	while (1) {
 		cargc = 0;
@@ -27,31 +33,35 @@ int main(int argc, char *argv[], char *envp[])
 		do {
 			printf("Ctrl-C to exit. Enter a command: ");
 			fgets(cmd,MAXCARGC,stdin);
+			infrom = "";
+			outto = "";
+			redirection = 0;
 			for (
 				cargc = 0, ctok = strtok(cmd," \n"); 
-				cargc < MAXCARGC ; 
-				ctok = strtok(NULL," \n"), cargc++
+				cargc < MAXCARGC; 
+				ctok = strtok(NULL," \n")
 			) {
-				if (ctok == NULL) {
-					cargv[cargc] = NULL;
-					break;
-				}
+				if (ctok == NULL) break;
+
 				cchar = ctok[0];
 				switch (cchar) {
 				/* case '|': ideally you'd want pipes to work recursively? */
 				case '<':
+					redirection = 1;
 					if (strlen(ctok+1) > 0) infrom = ctok+1;
 					else infrom = strtok(NULL," \n");
 					break;
 				case '>': 
+					redirection = 1;
 					if (strlen(ctok+1) > 0) outto = ctok+1;
 					else outto = strtok(NULL," \n");
 					break;
 				default: 
+					/* we assume with redirection that we have our command already */
+					if (redirection) break;
 					cchar = 0; 
-					cargv[cargc] = ctok;
+					cargv[cargc++] = ctok;
 				}
-				if (cchar) break;
 			}
 		} while (cargc == 0);
 
@@ -70,6 +80,30 @@ int main(int argc, char *argv[], char *envp[])
 		}
 
 		if (cpid == 0) { 		/* Code executed by child */
+
+			for (i=0; i<cargc; i++) {
+				fprintf(stderr, "arg[%d] %s\n", i, cargv[i]);
+			}
+			/* get input from a file */
+                        if (strlen(infrom)) {
+                                if ((infd = open(infrom, O_RDONLY)) == -1) {
+                                        perror("open input redirect");
+                                        exit(EXIT_FAILURE);
+                                }
+                                close(STDIN_FILENO);
+                                dup(infd);
+                        }
+
+			/* redirecting output to a file */
+                        if (strlen(outto)) {
+                                if ((outfd = open(outto, O_RDWR|O_CREAT,mode)) == -1) {
+                                        perror("open output redirect");
+                                        exit(EXIT_FAILURE);
+                                }
+                                close(STDOUT_FILENO);
+                                dup(outfd);
+                        }
+
 			execve(cargv[0], cargv, envp);
 			perror("execve");	/* execve() only returns on error */
 			exit(EXIT_FAILURE);
