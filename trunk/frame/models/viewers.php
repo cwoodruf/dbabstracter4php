5e42aa30bc001c1e58ed85ed1fedcb6c5ded5e1f
cwoodruf@gmail.com <cwoodruf@gmail.com@c5e326fc-19f7-11df-8006-ddd0014e6ec4>
2010-10-11 09:28:05 +0000

    added missing files

---
+++ trunk/frame/models/viewers.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * wrapper for viewers table base class
+ * viewers keeps track of the unique logins that have viewed a note
+ * example relation table
+ */
+class Viewers extends ViewersRelation {
+}
+
