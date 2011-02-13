6ba9176f202b70e64bc14fcf4a91aa5becea63d5
cwoodruf@gmail.com <cwoodruf@gmail.com@c5e326fc-19f7-11df-8006-ddd0014e6ec4>
2011-02-13 07:51:00 +0000

    remove templates_c junk

--- trunk/vmadam/views/templates_c/%%BE^BE5^BE5F030F%%phones.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-02-03 00:29:12
-         compiled from phones.tpl */ ?>
-<h1>Phones</h1>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "tools/dumpall.tpl", 'smarty_include_vars' => array('list' => $this->_tpl_vars['phones'])));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
--- trunk/vmadam/views/templates_c/%%EB^EB9^EB93DE89%%profiles.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-02-02 04:00:19
-         compiled from profiles.tpl */ ?>
-<h1>All profiles</h1>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "tools/dumpall.tpl", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
